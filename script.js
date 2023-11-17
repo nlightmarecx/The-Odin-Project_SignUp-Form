f4247ee3aadec7ef4a5c4b3f21ae6c6c73139376
nlightmarecx <lfromhwere@gmail.com>
2023-11-17 17:45:58 +0400

    create HTML skleeton, add imgs

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Greet the user function
+
+const name = document.querySelector("#username")
+const greetMeButton = document.querySelector(".greet-btn")
+const greetingOutput = document.querySelector(".greeting")
+
+greetMeButton.addEventListener('click', (event) => {
+   greetingOutput.innerText = `Hello ${name.value}`;
+})
+
+*/
